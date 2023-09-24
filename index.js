4ead84426015bff7d803737d7962407627364dd1
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2023-09-24 09:24:28 +0530

    multi email
    Signed-off-by: Gopisakthivel <gopisakthivel@alphabsolutions.com>

--- index.js
+++ index.js
@@ -8,17 +8,31 @@ const transporter= nodemailer.createTransport({
     }
 })
 
+const maillist =[
+    'babotis174@cdeter.com',
+    'bebiy40247@fandsend.com',
+    'xepefa5468@alvisani.com'
+]
+
 const mailOptions={
     form:'petchimuthuvoc@gmail.com',
-    to:'babotis174@cdeter.com',
+    // to:'babotis174@cdeter.com',
     subject:'Nodemailer Mail',
     text:'Testing Mail'
+    //  html: "<b>Hello world ✔</b>" // html body
 }
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    }else{
-        console.log('Email Send'+ info.response);
-    }
+maillist.forEach(function(to,i, array){
+    
+    mailOptions.to=to
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        }else{
+            console.log('Email Send'+ info.response + to);
+        }
+    })
+
 })
+
